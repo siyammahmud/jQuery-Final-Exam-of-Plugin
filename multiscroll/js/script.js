cfc1547911c5854421c81114e2f6e4d4d861afe1
siyammahmud <sayemchowdhury21@gmail.com>
2017-01-21 09:28:57 +0600

    Add files via upload

---
+++ multiscroll/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+            $('#myContainer').multiscroll({
+            	sectionsColor: ['#78CE23', '#7E8F7C', '#7BAD69'],
+            	anchors: ['first', 'second', 'third'],
+            	menu: '#menu',
+            	scrollingSpeed: 1000
+            });
+        });
+
+
